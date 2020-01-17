fd237a996039f07160f49889e94076fc58d0bb78
benyamynbrkyc <benjamin.brkic@gmail.com>
2020-01-17 17:41:57 +0100

    port mods

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
+
 const app = express();
 
-const PORT = process.env || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
