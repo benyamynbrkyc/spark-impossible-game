dfdfeae194d341bf0a27e9f6bc42c54e8199bf65
benyamynbrkyc <benjamin.brkic@gmail.com>
2020-01-17 17:38:58 +0100

    port setup

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const PORT = process.env || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is up on port 3000');
 });
